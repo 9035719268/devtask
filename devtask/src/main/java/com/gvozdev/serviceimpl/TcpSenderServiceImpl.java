38bab3c6b6e4137de8279618a928d137313f154c
Alexander <9035719268a@gmail.com>
2021-06-28 00:45:51 +0300

    - Fixed socket closing

--- devtask/src/main/java/com/gvozdev/serviceimpl/TcpSenderServiceImpl.java
+++ devtask/src/main/java/com/gvozdev/serviceimpl/TcpSenderServiceImpl.java
@@ -28,26 +28,30 @@ public class TcpSenderServiceImpl implements TcpSenderService {
     );
 
     @Override
-    public void sendData() throws IOException {
+    public void sendData() {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         ApplicationConfig applicationConfig = context.getBean("applicationConfig", ApplicationConfig.class);
 
-        Socket socket = new Socket(applicationConfig.getDestAddr(), applicationConfig.getDestPort());
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        try (Socket socket = new Socket(applicationConfig.getDestAddr(), applicationConfig.getDestPort())) {
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        bufferedWriter.write(strings.get(0));
-        bufferedWriter.newLine();
-        LOGGER.info(valueOf(encodeHex(strings.get(0).getBytes(UTF_8))));
+            bufferedWriter.write(strings.get(0));
+            bufferedWriter.newLine();
+            LOGGER.info(valueOf(encodeHex(strings.get(0).getBytes(UTF_8))));
 
-        bufferedWriter.write(strings.get(1));
-        bufferedWriter.newLine();
-        LOGGER.info(valueOf(encodeHex(strings.get(1).getBytes(UTF_8))));
+            bufferedWriter.write(strings.get(1));
+            bufferedWriter.newLine();
+            LOGGER.info(valueOf(encodeHex(strings.get(1).getBytes(UTF_8))));
 
-        bufferedWriter.write(strings.get(2));
-        bufferedWriter.newLine();
-        LOGGER.info(valueOf(encodeHex(strings.get(2).getBytes(UTF_8))));
+            bufferedWriter.write(strings.get(2));
+            bufferedWriter.newLine();
+            LOGGER.info(valueOf(encodeHex(strings.get(2).getBytes(UTF_8))));
 
-        bufferedWriter.write(strings.get(3));
-        LOGGER.info(valueOf(encodeHex(strings.get(3).getBytes(UTF_8))));
+            bufferedWriter.write(strings.get(3));
+            LOGGER.info(valueOf(encodeHex(strings.get(3).getBytes(UTF_8))));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
